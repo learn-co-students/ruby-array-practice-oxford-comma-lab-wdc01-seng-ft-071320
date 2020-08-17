d4180a99de6bbb8f8a9fc5a22c078b33350e9a64
alexstephane <alexngundji@gmail.comm.com>
2020-08-17 15:05:42 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+#     array.to_s
+    
+#     array.join(" and ")
+    
+#    #array.(",")
+    
 
+# end
+
+def oxford_comma(arr)
+    if arr.length == 1
+        arr.join
+    elsif arr.length == 2
+        arr.join(" and ")
+    else
+        
+        arr.each.collect do |value|
+            if value == arr[-1]
+                arr[-1] = "and " << value
+            end
+        end
+        arr.join(", ")
+    end
 end
